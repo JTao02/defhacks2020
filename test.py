1debe4791f6ea0e2b5883c2570833bb5e5a2b7b5
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-06-20 21:08:33 -0400

    vote kick terry

---
+++ test.py
@@ -0,0 +1,43 @@
+import requests
+from get_api_key import get_file_contents
+
+# Business Search - https://api.yelp.com/v3/businesses/search
+#
+
+# define a business ID
+business_id = 'zZ-ytyY5R_KGk1tJCE2f2g'
+
+API_KEY = get_file_contents('apikey')
+ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+HEADERS = {'Authorization': 'bearer %s' % API_KEY}
+
+# define the parameters
+PARAMETERS = {'term': 'Starbucks',
+              'limit': 50,
+              'offset': 50,
+              'radius': 40000,
+              'location': 'Ottawa'}
+
+# FULL LIST
+'''
+PARAMETERS = {'term': 'good food',
+              'location': 'San Diego',
+              'radius': 10000,
+              'categories': 'bars,french',
+              'locales': 'en_US',
+              'limit': 50,
+              'offset': 150,
+              'sort_by': 'best_match',
+              'price': '1',
+              'open_now': True,
+              'attributes': 'hot_and_new'
+              }
+'''
+
+# make a request to the yelp API
+response = requests.get(url = ENDPOINT, params = PARAMETERS, headers = HEADERS)
+
+business_data = response.json()
+
+for biz in business_data['businesses']:
+    print(biz['name'])
